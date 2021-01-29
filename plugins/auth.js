74d4be2de7f4468cdeccda0303519bc85237a32c
yoyo-checkout <stem597sag445@gmail.com>
2021-01-29 10:18:44 +0800

    fix router guard auth bug

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,10 +1,9 @@
 export default ({ app, store }) => {
   if (process.browser) {
-    const token = window.localStorage.getItem('token');
-    const expires = new Date(window.localStorage.getItem('expires'));
-    const now = new Date();
-
     app.router.beforeEach((to, from, next) => {
+      const token = window.localStorage.getItem('token');
+      const expires = new Date(window.localStorage.getItem('expires'));
+      const now = new Date();
       const whiteList = [
         'index',
         'courses',
@@ -14,7 +13,9 @@ export default ({ app, store }) => {
         if (to.name === 'index') {
           next({ name: 'account' });
         } else {
-          window.localStorage.setItem('expires', new Date(now.setDate(now.getDate() + 7)));
+          const newNow = new Date(now);
+
+          window.localStorage.setItem('expires', new Date(newNow.setDate(now.getDate() + 7)));
           next();
         }
       } else {
